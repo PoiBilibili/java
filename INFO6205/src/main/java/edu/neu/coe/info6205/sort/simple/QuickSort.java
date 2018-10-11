2b6257b2ebf3899770639b64c8b1106546423829
JingZhou <zhouruge@163.com>
2018-10-11 15:09:25 -0400

    implete the QuickSort

--- INFO6205/src/main/java/edu/neu/coe/info6205/sort/simple/QuickSort.java
+++ INFO6205/src/main/java/edu/neu/coe/info6205/sort/simple/QuickSort.java
@@ -5,13 +5,27 @@ import java.util.Arrays;
 public class QuickSort<X extends Comparable<X>> implements Sort<X> {
     @Override
     public void sort(X[] xs, int from, int to) {
-        Arrays.sort(xs, from, to);
-//        for (int i = from; i < to; i++)
-//            for (int j = i; j > 0; j--)
-//                if (less(xs[j], xs[j - 1]))
-//                    swap(xs, j, j - 1);
-//                else break;
-//
+        //TODO do shuffle in X[] xs
+    	quickSort(xs,from, to);
+    }
+    private void quickSort(X[] xs, int from, int to) {
+    	if(from >= to) return;
+    	int j = partitioning(xs, from, to);
+    	quickSort(xs,from, j-1);
+    	quickSort(xs, j+1, to);
+    }
+    private int partitioning(X[] xs, int from, int to) {
+    	int i = from, j =to+1;
+    	while(true) {
+    		while(Helper.less(xs[++i], xs[from]))
+    			if(i>=to) break;
+    		while(Helper.less(xs[from], xs[--j]))
+    			if(j<=0) break;
+    		if(i>=j) break;
+    		Helper.swap(xs, from, to, i, j);
+    	}
+    	Helper.swap(xs, from, to, from, j);
+    	return j;
     }
 }
 
